5a9398c5c3d993e3b7a4116af921315f23022e00
Taha Ahmed <taha@kommserv.se>
2022-02-27 19:33:53 +0100

    Upgrade from 2.5.12 to 3.1.8 completed.
    + to avoid increasing duplication of tasks across configure.yml and
      restore.yml I collapsed all database-touching logic into a new configure.yml
    + added systemd timer for housekeeping job
    + redis should use empty password (redis role does not configure local auth)

--- files/upgrade.sh
+++ files/upgrade.sh
@@ -1,35 +1,113 @@
 #!/bin/bash
 # This script will prepare NetBox to run after the code has been upgraded to
 # its most recent release.
-#
-# Once the script completes, remember to restart the WSGI service (e.g.
-# gunicorn or uWSGI).
 
-PYTHON="python3"
-PIP="pip3"
+# This script will invoke Python with the value of the PYTHON environment
+# variable (if set), or fall back to "python3". Note that NetBox v3.0+ requires
+# Python 3.7 or later.
 
-# TODO: Remove this in v2.6 as it is no longer needed under Python 3
-# Delete stale bytecode
-COMMAND="find . -name \"*.pyc\" -delete"
-echo "Cleaning up stale Python bytecode ($COMMAND)..."
-eval $COMMAND
+cd "$(dirname "$0")"
+VIRTUALENV="$(pwd -P)/venv"
+PYTHON="${PYTHON:-python3}"
 
-# Uninstall any Python packages which are no longer needed
-COMMAND="${PIP} uninstall -r old_requirements.txt -y"
-echo "Removing old Python packages ($COMMAND)..."
-eval $COMMAND
+# Remove the existing virtual environment (if any)
+if [ -d "$VIRTUALENV" ]; then
+  COMMAND="rm -rf ${VIRTUALENV}"
+  echo "Removing old virtual environment..."
+  eval $COMMAND
+else
+  WARN_MISSING_VENV=1
+fi
 
-# Install any new Python packages
-COMMAND="${PIP} install -r requirements.txt --upgrade"
-echo "Updating required Python packages ($COMMAND)..."
-eval $COMMAND
+# Create a new virtual environment
+COMMAND="${PYTHON} -m venv ${VIRTUALENV}"
+echo "Creating a new virtual environment at ${VIRTUALENV}..."
+eval $COMMAND || {
+  echo "--------------------------------------------------------------------"
+  echo "ERROR: Failed to create the virtual environment. Check that you have"
+  echo "the required system packages installed and the following path is"
+  echo "writable: ${VIRTUALENV}"
+  echo "--------------------------------------------------------------------"
+  exit 1
+}
+
+# Activate the virtual environment
+source "${VIRTUALENV}/bin/activate"
+
+# Upgrade pip
+COMMAND="pip install --upgrade pip"
+echo "Updating pip ($COMMAND)..."
+eval $COMMAND || exit 1
+pip -V
+
+# Install necessary system packages
+COMMAND="pip install wheel"
+echo "Installing Python system packages ($COMMAND)..."
+eval $COMMAND || exit 1
+
+# Install required Python packages
+COMMAND="pip install -r requirements.txt"
+echo "Installing core dependencies ($COMMAND)..."
+eval $COMMAND || exit 1
+
+# Install optional packages (if any)
+if [ -s "local_requirements.txt" ]; then
+  COMMAND="pip install -r local_requirements.txt"
+  echo "Installing local dependencies ($COMMAND)..."
+  eval $COMMAND || exit 1
+elif [ -f "local_requirements.txt" ]; then
+  echo "Skipping local dependencies (local_requirements.txt is empty)"
+else
+  echo "Skipping local dependencies (local_requirements.txt not found)"
+fi
 
 # Apply any database migrations
-COMMAND="${PYTHON} netbox/manage.py migrate"
+COMMAND="python3 netbox/manage.py migrate"
 echo "Applying database migrations ($COMMAND)..."
-eval $COMMAND
+eval $COMMAND || exit 1
+
+# Trace any missing cable paths (not typically needed)
+COMMAND="python3 netbox/manage.py trace_paths --no-input"
+echo "Checking for missing cable paths ($COMMAND)..."
+eval $COMMAND || exit 1
+
+# Build the local documentation
+COMMAND="mkdocs build"
+echo "Building documentation ($COMMAND)..."
+eval $COMMAND || exit 1
 
 # Collect static files
-COMMAND="${PYTHON} netbox/manage.py collectstatic --no-input"
+COMMAND="python3 netbox/manage.py collectstatic --no-input"
 echo "Collecting static files ($COMMAND)..."
-eval $COMMAND
+eval $COMMAND || exit 1
+
+# Delete any stale content types
+COMMAND="python3 netbox/manage.py remove_stale_contenttypes --no-input"
+echo "Removing stale content types ($COMMAND)..."
+eval $COMMAND || exit 1
+
+# Delete any expired user sessions
+COMMAND="python3 netbox/manage.py clearsessions"
+echo "Removing expired user sessions ($COMMAND)..."
+eval $COMMAND || exit 1
+
+if [ -v WARN_MISSING_VENV ]; then
+  echo "--------------------------------------------------------------------"
+  echo "WARNING: No existing virtual environment was detected. A new one has"
+  echo "been created. Update your systemd service files to reflect the new"
+  echo "Python and gunicorn executables. (If this is a new installation,"
+  echo "this warning can be ignored.)"
+  echo ""
+  echo "netbox.service ExecStart:"
+  echo "  ${VIRTUALENV}/bin/gunicorn"
+  echo ""
+  echo "netbox-rq.service ExecStart:"
+  echo "  ${VIRTUALENV}/bin/python"
+  echo ""
+  echo "After modifying these files, reload the systemctl daemon:"
+  echo "  > systemctl daemon-reload"
+  echo "--------------------------------------------------------------------"
+fi
+
+echo "Upgrade complete! Don't forget to restart the NetBox services:"
+echo "  > sudo systemctl restart netbox netbox-rq"
